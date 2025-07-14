0517375255655a94b4bc002833e1d3920e702387
Artem Korsakov <a.korsakov@kryptonite.ru>
2025-07-14 09:06:44 +0300

    Initial documentation

--- main.ts
+++ main.ts
@@ -1,39 +1,29 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PersonalDevelopmentPlanPlugin extends Plugin {
+	settings: PersonalDevelopmentPlanSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'PersonalDevelopmentPlan Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass('personal-development-plan-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('PersonalDevelopmentPlan Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-personal-development-plan-modal',
+			name: 'PersonalDevelopmentPlan (simple)',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new PersonalDevelopmentPlanModal(this.app).open();
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
@@ -56,7 +46,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new PersonalDevelopmentPlanModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,7 +56,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new PersonalDevelopmentPlanSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -91,7 +81,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class PersonalDevelopmentPlanModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -107,10 +97,18 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+interface PersonalDevelopmentPlanSettings {
+	setting1: string;
+}
 
-	constructor(app: App, plugin: MyPlugin) {
+const DEFAULT_SETTINGS: PersonalDevelopmentPlanSettings = {
+	setting1: 'default'
+}
+
+class PersonalDevelopmentPlanSettingTab extends PluginSettingTab {
+	plugin: PersonalDevelopmentPlanPlugin;
+
+	constructor(app: App, plugin: PersonalDevelopmentPlanPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -125,9 +123,9 @@ class SampleSettingTab extends PluginSettingTab {
 			.setDesc('It\'s a secret')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.setting1)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.setting1 = value;
 					await this.plugin.saveSettings();
 				}));
 	}
